c729dc8502790fc1454522297d326c1e7962d7ea
João Victor Nascimento <joao.victor@salesoft.com.br>
2023-07-13 22:03:27 -0300

    create basic component for fill names

--- src/components/FillNames.vue
+++ src/components/FillNames.vue
@@ -2,7 +2,40 @@
 </script>
 <template>
     <div>
-        <input type="text" placeholder="Novo nome para lista">
-        <button>Adicionar nome</button>
+        <div class="fields">
+            <input type="text" placeholder="Novo nome para lista">
+            <button>Adicionar nome</button>
+        </div>
+        <div class="filled-names">
+            <div class="name">
+                <span>Nome 1</span>
+                <button>Remover</button>
+            </div>
+            <div class="name">
+                <span>Nome 2</span>
+                <button>Remover</button>
+            </div>
+            <div class="name">
+                <span>Nome 3</span>
+                <button>Remover</button>
+            </div>
+        </div>
     </div>
 </template>
+<style scoped lang="scss">
+.fields {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1.5rem;
+    input {
+        font-size: 1.5rem;
+    }
+}
+.filled-names {
+    .name {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 1rem;
+    }
+}
+</style>
